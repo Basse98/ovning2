8dd2f78d7f9f667a03c7f87311fcaae9eda43c12
BobL <42844572+Bob-Utveckling@users.noreply.github.com>
2018-11-14 09:52:01 +0100

    Added one method to program

--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,10 @@ public class Program {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+         Hello();
     }
 
+    public static void Hello() {
+	    System.out.println ("Hi! Hello!");
+    }
 }
